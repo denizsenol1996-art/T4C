8e62e78a90a7a195cb60ca43353360ccf22f562b
Deniz Senol <denizsenol1996@gmail.com>
2026-03-23 22:32:22 +0000

    Cache bust: SW v10.16.0 + taxatie.js v20260323

--- sites/cardatax/m/sw.js
+++ sites/cardatax/m/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cdx-v10.8.5';
+const CACHE = 'cdx-v10.16.0';
 const URLS = ['/m/', '/m/manifest.json', '/m/icon-192.png', '/m/logo-cardatax.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS))) });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())) });
